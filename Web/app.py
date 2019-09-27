64ebff0ca1cb207d682ea2b7b181d5f9725ec721
Deep Mehta <deepme987@gmail.com>
2019-09-28 04:12:21 +0530

    File changes

--- Web/app.py
+++ Web/app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-
+    
 @app.route('/index', methods=['POST', 'GET'])
 def index():
-	print("Index")
-	if request.method == 'POST':
-		print("Hi")
-		return render_template('index.html', test="test")
-	# return "hello world"
+    print("Index")
+    if request.method == 'POST':
+        print("Hi")
+        title1= request.form['title']
+        domain1=request.form['link']
+        print(domain1)
+        print(title1)
+        return render_template('index.html', CONTEXT={'test': title1, 'do': domain1})
+    # return "hello world"
 # def redirect():
-#	 message = request.form['title']
-#	 domain = request.form['link']
-#	 return(message)
+#     message = request.form['title']
+#     domain = request.form['link']
+#     return(message)
 # def run(message,domain):
-#	return 'welcome %s' % name
-	return render_template("index.html")
+#    return 'welcome %s' % name
+    return render_template("index.html", CONTEXT=None)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
