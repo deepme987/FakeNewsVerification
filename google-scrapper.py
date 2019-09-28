86f6802a329f03790f6459a5c967c3829d717593
Deep Mehta <deepme987@gmail.com>
2019-09-28 12:41:35 +0530

    Test

--- google-scrapper.py
+++ google-scrapper.py
@@ -1,16 +1,56 @@
-import bs4 as bs
-import requests
-from googlesearch import search
+
+# from bs4 import BeautifulSoup
+# import urllib.request
+# from googlesearch import search
+from google import google
+import nltk.stem.snowball
+from nltk.stem import WordNetLemmatizer
+import string
+
+
+def remove_stopwords(title):
+    global stopwords, lemmatizer
+
+    title = set([x for x in title.split()]).difference(stopwords)
+    title = " ".join(title).lower()
+    for char in [',', '.', '\'', '!', ':']:
+        title = title.replace(char, '')
+    title = [lemmatizer.lemmatize(x) for x in title.split()]
+
+    return " ".join(title)
 
 
 def search_query(query):
     print("Searching for '{}' over internet".format(query))
+    results = [result for result in google.search(query, 1)]
+    # results.pop(2)
+    score = dict()
 
-    for result in search(query, tld="co.in", num=10, stop=10):
+    query = remove_stopwords(query)
+
+    print(results)
+    for i, result in enumerate(results):
+        # score = dict()
+        # source = urllib.request.urlopen(result).read()
+        # soup = BeautifulSoup(source, 'lxml')
+        result = remove_stopwords(result.description)
         print(result)
+        for val in query.split():
+            if i in score:
+                if val in result:
+                    # print(val, result)
+                    score[i] += 1
+            else:
+                score[i] = 0
 
-    
+    print(score)
+    print(query)
 
 
 if __name__ == '__main__':
+    stopwords = nltk.corpus.stopwords.words('english')
+    stopwords.extend(string.punctuation)
+    # stopwords.extend(['', '.', ','])
+    stopwords = set(stopwords)
+    lemmatizer = WordNetLemmatizer()
     search_query(input("Enter a search query: "))
